c06a65e810ac88d85897b3bf2b7b83fc805fb133
Connor Horwath <chorwath@butler.edu>
2025-11-30 19:17:55 -0600

    adding exercise controller, and exericse model todo add rest of crud methods for the controller

---
+++ Controllers/ExercisesController.cs
@@ -0,0 +1,52 @@
+﻿using gym.Models;
+using Gym.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace gym.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ExercisesController : ControllerBase
+    {
+        private readonly GymDbContext _context;
+
+        public ExercisesController(GymDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Exercise>>> GetExercises()
+        {
+            return Ok(await _context.Exercises.ToListAsync());
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<Exercise>> GetExercise(int id)
+        {
+            var exercise = await _context.Exercises.FindAsync(id);
+            if (exercise == null)
+                return NotFound();
+
+            return Ok(exercise);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Exercise>> AddExercise(Exercise exercise)
+        {
+            if (exercise == null)
+                return BadRequest();
+
+            _context.Exercises.Add(exercise);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(AddExercise), new {id = exercise.ExerciseID}, exercise);
+        }
+
+        //todo add rest of crud methods
+
+    }
+}
